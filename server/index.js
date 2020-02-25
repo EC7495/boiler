29d1353f94b3da877e148c51443073528af21ebc
Erick <ecanals07@gmail.com>
2020-02-24 21:14:33 -0500

    error handling

--- server/index.js
+++ server/index.js
@@ -14,6 +14,20 @@ app.use(express.urlencoded({ extended: true }));
 // static files
 app.use(express.static(path.join(__dirname, '../public')));
 
+// api routes
+app.use('/api', require('./api'));
+
+// send index.html for any other requests
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// error handling middleware
+app.use((err, req, res, next) => {
+  if (process.env.NODE_ENV !== 'test') console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error');
+});
+
 app.listen(PORT, () => {
   console.log(`Listening of port ${PORT}`);
 });
