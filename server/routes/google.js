203c119bf3392210fdb61e563e6954055f8b3ddf
Erick <ecanals07@gmail.com>
2020-02-28 18:41:34 -0500

    finished going through workshop (minus tests) compiles successfully, need to test in front end. (login, logout, signup routes working as well)

--- server/routes/google.js
+++ server/routes/google.js
@@ -1,6 +1,46 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('../../.secrets');
+}
+
 const passport = require('passport');
 const router = require('express').Router();
+const User = require('../db/models/user');
 
 router.get('/', passport.authenticate('google', { scope: 'email' }));
+
+router.get(
+  '/callback',
+  passport.authenticate({
+    successRedirect: '/',
+    failureRedirect: '/login'
+  })
+);
+
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const googleConfig = {
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackUrl: '/auth/google/callback'
+};
+const googleStrategy = new GoogleStrategy(
+  googleConfig,
+  async (token, refreshToken, profile, done) => {
+    try {
+      const defaults = {
+        email: profile.emails[0].value,
+        googleId: profile.id
+      };
+      const [user] = await User.findOrCreate({
+        where: { googleId: profile.id },
+        defaults: defaults
+      });
+      done(null, user);
+    } catch (error) {
+      done(error);
+    }
+  }
+);
+
+passport.use(googleStrategy);
 
 module.exports = router;
