00ce3c6e27e1d0cae09a7786facd5b4cc0cd93f6
Erick <ecanals07@gmail.com>
2020-02-28 01:16:03 -0500

    login, logout, signup, me routes done

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
 const User = require('../db/models/user');
 
+router.get('/me', (req, res, next) => {
+  try {
+    res.json(req.user || {});
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.put('/login', async (req, res, next) => {
   try {
     const user = await User.findOne({ where: { email: req.body.email } });
@@ -9,6 +17,27 @@ router.put('/login', async (req, res, next) => {
     else {
       req.login(user, error => (error ? next(error) : res.json(user)));
     }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    const user = await User.create({
+      email: req.body.email,
+      password: req.body.password
+    });
+    req.login(user, error => (error ? next(error) : res.json(user)));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/logout', (req, res, next) => {
+  try {
+    req.logout();
+    req.session.destroy(error => (error ? next(error) : res.sendStatus(204)));
   } catch (error) {
     next(error);
   }
